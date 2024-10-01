db9deae3c32b1800b9a79a8b03028fccfed7e2a8
DEV <DEV@DESKTOP-8VP4IIT>
2024-10-01 15:57:44 +0300

    bolum7 ileri dart

---
+++ Bolum7 İleri Dart/asenkron_islemler/android/app/src/main/kotlin/com/info/asenkron_islemler/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.info.asenkron_islemler
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ Bolum7 İleri Dart/ileri_dart/android/app/src/main/kotlin/com/info/ileri_dart/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.info.ileri_dart
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
